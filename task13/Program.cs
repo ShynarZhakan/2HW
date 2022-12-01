bfe215389a1e57a742c1a3829b73013a3f2a4f84
ShynarZhakan <schubblezam@gmail.com>
2022-12-01 19:39:09 +0600

    committed 2HW folder

---
+++ task13/Program.cs
@@ -0,0 +1,17 @@
+﻿// 13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.Через строку решать нельзя.
+
+List<int> list = new List<int>();
+        Console.WriteLine("Введите число: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        while (n > 0) {
+            int res = n % 10;
+            n = n / 10;
+            list.Add(res);
+        }
+        int[] terms = list.ToArray();
+        Array.Reverse(terms);
+        if(2 < terms.Length) {
+            Console.WriteLine(terms[2]);
+        } else {
+            Console.WriteLine("Третьей цифры числа нет");
+        }
---
+++ taskAdditional2/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Сколько программистов?");
+int number = Convert.ToInt32(Console.ReadLine());
+string x1 = Convert.ToString("программист");
+string x2 = Convert.ToString("а");
+string x3 = Convert.ToString("ов");
+
+if (number % 10 == 0 || number % 10 == 5 || number % 10 == 6 || number % 10 == 7 || number % 10 == 8 || number % 10 == 9)
+{
+    Console.WriteLine($"{number} {x1}{x3}");
+}
+
+else if ( number % 100 == 11 || number % 100 == 12 || number % 100 == 13 || number % 100 == 14)
+{
+    Console.WriteLine($"{number} {x1}{x3}");
+}
+
+else if (number % 10 == 2 || number % 10 == 3 || number % 10 == 4)
+{
+    Console.WriteLine($"{number} {x1}{x2}");
+}
+
+else if (number % 10 == 1)
+{
+    Console.WriteLine($"{number} {x1}");
+}
+
